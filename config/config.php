cb65944a7313aa660311f170cbd460c46f4df661
innosAbel <innosabel@gmail.com>
2021-03-11 14:33:36 +0100

    setup remote database

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
-define('DB_NAME','counter');
+define('DB_HOST','sql6.freemysqlhosting.net');
+define('DB_USER','sql6398220');
+define('DB_PASS','kJF8A3F7G9');
+define('DB_NAME','sql6398220');
 
 $site_description = '';
