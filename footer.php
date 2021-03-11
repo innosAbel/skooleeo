aa687b4c83e3bba4bd1a0e2a1b5b729ef53e6ef8
innosAbel <innosabel@gmail.com>
2021-03-11 15:31:26 +0100

    restructuring still

--- footer.php
+++ footer.php
@@ -8,33 +8,7 @@
             <?php include 'counter.php'; ?>
             <hr>
             <h4>Page Views</h4>
-            <?php 
-              // connect to the MySQL server
-              $host = 'localhost';
-              $user = 'root';
-              $pass = '';
-              $dbname = 'counter';
-              $conn = new mysqli($host, $user, $pass, $dbname);
-
-              //Page URL 
-              $page_url = "http://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-              
-              //check for count
-              $query = mysqli_query($conn, "SELECT * FROM `pageview` WHERE `pageurl`='".$page_url."' "); 
-              $rows = mysqli_fetch_array($query); 
-              $counter = $rows['totalview']; 
-              if(empty($counter)) 
-              {
-              $counter = 1;
-              mysqli_query($conn, "INSERT INTO `pageview` SET `pageurl`='".$page_url."',`totalview`='".$counter."'"); 
-              } 
-              //increase counters by 1 on page load
-              $counter = $counter+1;
-              mysqli_query($conn, "UPDATE `pageview` SET `totalview`='".$counter."' WHERE `pageurl`='".$page_url."'");
-              if($counter)
-              {
-              echo "Total page visit: ".$counter;
-              } ?>
+            <?php include 'pageviews.php'; ?>
             <p><?php echo $site_description ;?></p>
             </div>
           </div>
